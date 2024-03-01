c1f9e544197b6dce2e2bcb62fc730a3e8e4ca1de
Kim Thanh Bui <thanh.kimm.bui@gmail.com>
2024-02-29 21:32:05 -0800

    Initial Commit

---
+++ Destini/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Kim Thanh Bui on 2/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
